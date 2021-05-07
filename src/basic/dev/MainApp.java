c632b3643911ca6a75327e585f58c95d4a7cc145
TranQuocHuy20CDTH41 <82139951+TranQuocHuy20CDTH41@users.noreply.github.com>
2021-05-07 12:01:23 +0700

    Update MainApp.java

--- src/basic/dev/MainApp.java
+++ src/basic/dev/MainApp.java
@@ -62,29 +62,22 @@ public class MainApp {
 		    } else {
 		                  System.out.printf("So %d kp la so Polinom",temp);
 		               //tong cac so tu 100-999 co tong 3 chu so chia het cho 3   
-		                int tempMod = 0;
-		      			int tempDiv = 0;
-		      			int M  = 0;
+		                int tempMod = 0, tempDiv = 0, M = 0;
 		      			System.out.println("\nCac so tu 100 - 999 co tong cac chu so chia het cho 3 la:");
 		      			
 		      			for(int i1 = 100; i1 < 1000 ; i1++) {
 		      				tempDiv = i1;
-		      				sum = 0;
+		      				M = 0;
 		      				
 		      				while(tempDiv > 0 ) {	
 		      				    tempMod = tempDiv%10;
 		      				    tempDiv = tempDiv/10;
-		      				    sum +=tempMod;
+		      				    M += tempMod;
 		      				}
-		      				
-		      				
-		      				if(sum%3 == 0) {
-		      					System.out.printf(String.format("%d\t",i1));
-		      				}
-		      				
-		      			}
-		      }
+		      				if(M % 3 == 0) 
+		      					System.out.printf(String.format("%d\t", i1));	
+		      		}
+		      	}
 		}
-		
 	}
 }
